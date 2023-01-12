5a98b64bfc933144723e5957f8399908d7782c55
Giniatsu <aragarcia@addu.edu.ph>
2023-01-13 02:03:06 +0800

    MovieWeeb project created

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
+import SearchIcon from './search.svg';
 
-/* const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastname}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  );
-} */
+const API_URL = "https://www.omdbapi.com?apikey=37c5f80";
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-  useEffect(() => {
-    alert('Counter changed' + counter);
-  }, [counter]);
-/*   const name = null;
-  const isNameShowing = true; */
+        setMovies(data.Search);
+    }
+
+    //for checking the value in movie api in the console
+    useEffect(() => {
+        searchMovies("Spiderman");
+    }, []);
+    //
+
   return (
-    <div className="App">
-      {/* <Person name={'JJ gae'} lastname={'Jeje'} age={'90'}/>
-      <Person name={'Gabe gwafu'} age={10+12}/>
-      <h1>React {isNameShowing ? name : 'someone'}</h1>
-      {name ? (
-      <>
-        test
-      </>
-      ): (
-        <h1>Not showing</h1>
-      )} */}
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <div className="app">
+      <h1>MovieWeeb</h1>
+      <div className="search">
+        <input 
+            placeholder="Search for a movie..." 
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        <img
+            src={SearchIcon}
+            alt="Search"
+            onClick={() => searchMovies(searchTerm)}
+            />
+      </div>
+      {
+        movies?.length > 0 
+        ? (
+            <div className="container">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} />
+                ))}
+            </div>
+        ) :
+        (
+            <div className="empty">
+                <h2>No Movies Found</h2>
+            </div>
+        )
+      }
+      
     </div>
   );
 }
